dced65bd3b88256ae948ef1d6d429468049dac98
Matt Warrick <matt@documatt.com>
2023-08-07 15:28:46 +0200

    primevue and primeblock example

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,179 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { ref } from "vue";
+
+import { useToast } from "primevue/usetoast";
+
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Toast from "primevue/toast";
+
+const text = ref();
+
+const toast = useToast();
+
+const greet = () => {
+  toast.add({
+    severity: "info",
+    summary: "Summary",
+    detail: `I am a detail. Text is '${text.value}'`
+  });
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <Toast></Toast>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="txt" v-model="text" />
+      <label for="txt">Username</label>
+    </span>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <Button label="Greet" @click="greet" icon="pi pi-user"></Button>
+  </div>
+
+  <div>
+    <div class="surface-ground px-4 py-8 md:px-6 lg:px-8">
+      <div class="text-900 font-bold text-6xl mb-4 text-center">
+        Pricing Plans
+      </div>
+      <div class="text-700 text-xl mb-6 text-center line-height-3">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam
+        eligendi quos.
+      </div>
+
+      <div class="grid">
+        <div class="col-12 lg:col-4">
+          <div class="p-3 h-full">
+            <div
+              class="shadow-2 p-3 h-full flex flex-column surface-card"
+              style="border-radius: 6px"
+            >
+              <div class="text-900 font-medium text-xl mb-2">Basic</div>
+              <div class="text-600">Plan description</div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <div class="flex align-items-center">
+                <span class="font-bold text-2xl text-900">$9</span>
+                <span class="ml-2 font-medium text-600">per month</span>
+              </div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <ul class="list-none p-0 m-0 flex-grow-1">
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Arcu vitae elementum</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Dui faucibus in ornare</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Morbi tincidunt augue</span>
+                </li>
+              </ul>
+              <hr
+                class="mb-3 mx-0 border-top-1 border-none surface-border mt-auto"
+              />
+              <Button label="Buy Now" class="p-3 w-full mt-auto"></Button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 lg:col-4">
+          <div class="p-3 h-full">
+            <div
+              class="shadow-2 p-3 h-full flex flex-column surface-card"
+              style="border-radius: 6px"
+            >
+              <div class="text-900 font-medium text-xl mb-2">Premium</div>
+              <div class="text-600">Plan description</div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <div class="flex align-items-center">
+                <span class="font-bold text-2xl text-900">$29</span>
+                <span class="ml-2 font-medium text-600">per month</span>
+              </div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <ul class="list-none p-0 m-0 flex-grow-1">
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Arcu vitae elementum</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Dui faucibus in ornare</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Morbi tincidunt augue</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Duis ultricies lacus sed</span>
+                </li>
+              </ul>
+              <hr class="mb-3 mx-0 border-top-1 border-none surface-border" />
+              <Button label="Buy Now" class="p-3 w-full"></Button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 lg:col-4">
+          <div class="p-3 h-full">
+            <div
+              class="shadow-2 p-3 flex flex-column surface-card"
+              style="border-radius: 6px"
+            >
+              <div class="text-900 font-medium text-xl mb-2">Enterprise</div>
+              <div class="text-600">Plan description</div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <div class="flex align-items-center">
+                <span class="font-bold text-2xl text-900">$49</span>
+                <span class="ml-2 font-medium text-600">per month</span>
+              </div>
+              <hr class="my-3 mx-0 border-top-1 border-none surface-border" />
+              <ul class="list-none p-0 m-0 flex-grow-1">
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Arcu vitae elementum</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Dui faucibus in ornare</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Morbi tincidunt augue</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Duis ultricies lacus sed</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Imperdiet proin</span>
+                </li>
+                <li class="flex align-items-center mb-3">
+                  <i class="pi pi-check-circle text-green-500 mr-2"></i>
+                  <span>Nisi scelerisque</span>
+                </li>
+              </ul>
+              <hr class="mb-3 mx-0 border-top-1 border-none surface-border" />
+              <Button
+                label="Buy Now"
+                class="p-3 w-full p-button-outlined"
+              ></Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+
+import "./assets/main.css";
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/lara-light-teal/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  ripple: true
+});
+app.use(ToastService);
 
 app.mount("#app");
