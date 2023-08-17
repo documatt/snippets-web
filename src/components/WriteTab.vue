5bf03fef31159174f7b8351a45095e857a1324bb
Matt Warrick <matt@documatt.com>
2023-08-17 17:07:54 +0200

    explorer component dummy

--- src/components/WriteTab.vue
+++ src/components/WriteTab.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import Editor from "./Editor.vue";
 import EditorToolbar from "./EditorToolbar.vue";
+import Explorer from "./Explorer.vue";
 import OfferSampleDoc from "./OfferSampleDoc.vue";
 import PreviewSidebar from "./PreviewSidebar.vue";
 </script>
 
 <template>
   <div class="grid">
-    <div class="col-12 xl:col-8 xl:col-offset-2">
+    <div class="col-12 xl:col-3">
+      <Explorer/>
+    </div>
+    <div class="col-12 xl:col-9">
       <EditorToolbar />
       <OfferSampleDoc />
       <Editor />
