3c4dcfe86d635a25ca570132db143627860e1803
Matt Warrick <matt@documatt.com>
2023-09-12 21:46:54 +0200

    full screen layout with footer

--- components/Writer.vue
+++ components/Writer.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="grid grid-nogutter">
-    <div class="col-12" style="height: 10vh;">
+  <div class="fullscreen">
+    <div class="toolbar mb-3">
       <WriterToolbar/>
     </div>
-    <div class="col-12" style="height: 90vh; padding-bottom: 1rem;">
+    <div class="panes mb-3">
       <WriterPanes/>
+    </div>
+    <div class="footer">
+      <LayoutFooter/>
     </div>
   </div>
 </template>
@@ -12,4 +15,28 @@
 <script setup lang="ts"></script>
 
 <style scoped>
+/* Grid-based variant from https://codyloyd.com/2021/full-screen/ */
+.fullscreen {
+  /* Hack for iOS*/
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: column;
+}
+.panes {
+  flex-grow: 1;
+  overflow: auto;
+}
+.toolbar {
+  height: max-content;
+  flex-shrink: 0;
+}
+.footer {
+  height: max-content;
+  flex-shrink: 0;
+}
 </style>
