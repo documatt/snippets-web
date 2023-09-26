56fa3fa18c35ae11ec3e07679f5c0fb9e933f41d
Matt Warrick <matt@documatt.com>
2023-09-26 03:01:02 +0200

    nprogress no spinner

--- src/utils/conf.ts
+++ src/utils/conf.ts
@@ -1,8 +1,12 @@
 /**
  * All conf loading and logic here
  */
+import NProgress from 'nprogress'
 
 export default {
   SENTRY_ENV: import.meta.env.VITE_SENTRY_ENV,
   SENTRY_ENABLED: import.meta.env.VITE_SENTRY_ENABLED === 'false' ? false : true,
 }
+
+// Only bar on the top, not spinner on the right upper corner
+NProgress.configure({showSpinner: false})
